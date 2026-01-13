0bf5af8c4873d54536ebf94b653b54780581f2e7
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-01-13 13:33:27 -0300

    adicionando responsividade a home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,51 +1,60 @@
 // css
-import styles from"./Home.module.css"
+import styles from "./Home.module.css"
 
-//react-router-dom
-import { useNavigate,Link } from "react-router-dom"
+// react-router-dom
+import { useNavigate, Link } from "react-router-dom"
 
-//react
+// react
 import { useState } from "react"
 import { useFetchDocuments } from "../../hooks/useFetchDocuments"
 
-//components
+// components
 import PostDetails from "../../components/PostDetails"
 
-
 const Home = () => {
-  const [query, setQuery] = useState('')
-  const {documents: posts, loading} = useFetchDocuments("posts")
+  const [query, setQuery] = useState("")
+  const { documents: posts, loading } = useFetchDocuments("posts")
 
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    if(query) {
-      return navigate(`/search?q=${query}`)
+    if (query) {
+      navigate(`/search?q=${query}`)
     }
   }
 
   return (
     <div className={styles.home}>
-        <h1>Veja os nossos posts mais recentes</h1>
-        <form onSubmit={handleSubmit} className={styles.search_form}>
-          <input type="text" placeholder="Ou busque por tags..." value={query} onChange={(e)=>setQuery(e.target.value)}/>
-          <button className="btn btn-dark">Pesquisar</button>
-        </form>
-        <div>
-          
-          {loading && <p>Carregando</p>}
+      <h1>Veja os nossos posts mais recentes</h1>
 
-          {posts  && posts.map((post) => <PostDetails key={post.id} post={post} />)}
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
 
-          {posts && posts.length === 0 && (
-            <div className={styles.noposts}>
-              <p>Não foram encontrados posts</p>
-              <Link to="/posts/create" className="btn">Criar primeiro post</Link>
-            </div>
-          )}
-        </div>
+      <div className={styles.posts}>
+        {loading && <p>Carregando...</p>}
+
+        {posts && posts.map((post) => (
+          <PostDetails key={post.id} post={post} />
+        ))}
+
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
